fdc47987742a34620f12a706aca426d5fa82ce75
TopTopsEnric <120408745+TopTopsEnric@users.noreply.github.com>
2025-02-16 22:41:39 +0100

    fdsf

--- api-supabase/api/index.js
+++ api-supabase/api/index.js
@@ -1,5 +1,6 @@
+
+require("dotenv").config();
 const app = require('../app');
-const port = process.env.PORT || 3000;
 
 // Para Vercel, necesitamos exportar la configuración del handler
 module.exports = app;
