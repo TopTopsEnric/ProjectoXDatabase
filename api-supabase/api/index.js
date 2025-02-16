c14448c27925eaa046aa13b80028eb578c4500c6
TopTopsEnric <120408745+TopTopsEnric@users.noreply.github.com>
2025-02-16 20:13:21 +0100

    pordiosfunciona

--- api-supabase/api/index.js
+++ api-supabase/api/index.js
@@ -4,9 +4,4 @@ const port = process.env.PORT || 3000;
 // Para Vercel, necesitamos exportar la configuración del handler
 module.exports = app;
 
-// Si estamos en desarrollo local, podemos iniciar el servidor
-if (process.env.NODE_ENV !== 'production') {
-    app.listen(port, () => {
-        console.log(`Server running on port ${port}`);
-    });
-}
+
